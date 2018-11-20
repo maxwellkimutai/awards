06ed36a88f776988f11c988ab3e0f7a0bd27b163
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-20 17:26:47 +0300

    add admin.py

--- award/admin.py
+++ award/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Project, Profile,Vote
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Vote)

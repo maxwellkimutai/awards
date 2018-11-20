d96dc3169260f57d92f073c21a67df331fcdb27c
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-20 17:29:32 +0300

    add project urls

--- awards/urls.py
+++ awards/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from django_registration.backends.one_step.views import RegistrationView
 from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'',include('award.urls'))
+    url(r'',include('award.urls')),
+    url(r'^accounts/register/',
+        RegistrationView.as_view(success_url='/accounts/login'),
+        name='django_registration_register'),
+    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'},name='logout')
 ]
